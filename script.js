eabb904d5a00af036fad74836106af902e93ccbd
01386141 <wenjunlei@sfmail.sf-express.com>
2021-04-10 22:35:42 +0800

    feat: 打两种包

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ async function start() {
     await execpromis('rm -rf ./lib');
     console.log(`lib文件夹删除成功`);
     console.log(`ts 开始打包`);
-    await execpromis('tsc');
+    await execpromis('tsc -m commonjs');
+    await execpromis('mv ./lib/src ./lib/common');
+    await execpromis('tsc -m esnext');
+    await execpromis('mv ./lib/src ./lib/esm');
     console.log(`ts 打包成功`);
   } catch (error) {
     console.error(`electron启动失败: ${error}`);
